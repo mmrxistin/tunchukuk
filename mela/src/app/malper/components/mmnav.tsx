14f8e4e6f1a3223198589e5c929eb924dd10352e
mmrxistin <mmrxistin@gmail.com>
2026-01-15 20:44:57 +0300

    SUPHANALLAH VELHAMDULILLAH ALLAHU EKBER

--- mela/src/app/malper/components/mmnav.tsx
+++ mela/src/app/malper/components/mmnav.tsx
@@ -1,6 +1,6 @@
 // Bismillahirrahmanirrahim
 // Elhamdulillahirabbulalemin
-// Ve salatu ve selamu ala resulina Muhammedin ve alihi ve sahbihi ecmain
+// Ve salatu ve selamu ala resulina Muhammedin 
 // Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
 // Subhanallah, Elhamdulillah, Allahu Ekber
 // Estağfirullah El-Azim
@@ -15,33 +15,31 @@ function Mmmnavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/malper">Nimarid</Navbar.Brand>
+        <Navbar.Brand href="/malper">Eroğlu İnşaat</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/malper">Anasayfa</Nav.Link>
-            <NavDropdown title="Projelerimiz" id="projects-nav-dropdown">
+            <NavDropdown title="Projeler" id="projects-nav-dropdown">
               <NavDropdown.Item href="/malper/projeler/konut">Konut Projeleri</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/projeler/ofis">Ofis & Ticari Alanlar</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/projeler/kafe">Kafe & Restoran</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/projeler/otel">Otel & Konaklama</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/projeler/tadilat">Tadilat & Renovasyon</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/projeler/ticari">Ticari & Endüstriyel</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="/malper/projeler/renovasyon">Renovasyon</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/projeler/anahtar-teslim">Anahtar Teslim</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Hizmetlerimiz" id="services-nav-dropdown">
-              <NavDropdown.Item href="/malper/hizmetler/tasarim">İç Mekan Tasarımı</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/hizmetler/uygulama">Uygulama & Proje Yönetimi</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/hizmetler/danisma">Danışmanlık</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/malper/hizmetler/3d">3D Görselleştirme</NavDropdown.Item>
+            <NavDropdown title="Hizmetler" id="services-nav-dropdown">
+              <NavDropdown.Item href="/malper/hizmetler/siva">Sıva & Cephe</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/hizmetler/boya">Boya & Kaplama</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/hizmetler/tesisat">Elektrik & Sıhhi Tesisat</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/hizmetler/fayans">Fayans & Zemin</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/hizmetler/cati">Çatı & İzolasyon</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="/malper/hakkimizda">Hakkımızda</Nav.Link>
             <Nav.Link href="/malper/ekip">Ekibimiz</Nav.Link>
             <Nav.Link href="/malper/iletisim">İletişim</Nav.Link>
-            <NavDropdown title="Blog" id="blog-nav-dropdown">
-              <NavDropdown.Item href="/malper/blog/dekorasyon">Dekorasyon Fikirleri</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/blog/trendler">Trendler</NavDropdown.Item>
-              <NavDropdown.Item href="/malper/blog/ipuclari">İpuçları</NavDropdown.Item>
+            <NavDropdown title="Bilgi" id="blog-nav-dropdown">
+              <NavDropdown.Item href="/malper/bilgi/ipuclari">İpuçları & Rehber</NavDropdown.Item>
+              <NavDropdown.Item href="/malper/bilgi/trendler">Sektör Haberleri</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
