711ce1fa416d583b2d4664c9e7c86a02552a1297
mmrxistin <mmrxistin@gmail.com>
2026-01-18 13:32:48 +0300

    SUPHANALLAH VELHAMDULILLAH ALLAHU EKBER

---
+++ mela/src/app/malper/parvekirin/page.tsx
@@ -0,0 +1,30 @@
+// Bısmillahirrahmanirrahim
+// Elhamdulillahirabbulalemin
+// Ve salatu ve selamu ala resulina Muhammedin 
+// Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
+// Subhanallah, Elhamdulillah, Allahu Ekber
+// Estağfirullah El-Azim
+
+
+
+import React from 'react'
+import PostEditor from '@/components/posts/editor/PostEditor'
+import ForYouFeed from '@/app/(main)/ForYouFeed'
+
+export default function page() {
+  return (
+    <main className="container mx-auto py-8">
+      <h1 className="text-2xl font-semibold mb-4">Paylaşımlar</h1>
+
+      {/* Post editor (client) */}
+      <section className="mb-6">
+        <PostEditor />
+      </section>
+
+      {/* Feed */}
+      <section>
+        <ForYouFeed />
+      </section>
+    </main>
+  )
+}
