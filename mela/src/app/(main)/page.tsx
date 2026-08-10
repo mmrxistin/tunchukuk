cb61866e08c07bbab3f86ab0b35206d0ae7eb545
mmrxistin <mmrxistin@gmail.com>
2026-08-10 13:23:08 +0300

    Suphan Allah ul Azim i Haq i Ehed i Xaliq i Şekur i  Vehhab i Quddus i Kerim ve Bihamdihi Velhamdulillah

--- mela/src/app/(main)/page.tsx
+++ mela/src/app/(main)/page.tsx
@@ -1,3 +1,13 @@
+// Bismillahirrahmanirrahim
+// Elhamdulillahi Rabbil Alamin
+// Essalatu vesselamu ala Resulina Muhammedin 
+// Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illAllah
+// Subhanallah, Elhamdulillah, Allahu Ekber
+// La ilahe illAllah, Muhammedur Resulullah
+// Allah U Ekber ve lillahi'l-hamd
+
+
+
 import AdminShell from "./components/AdminShell";
 import AdminDashboard from "./components/AdminDashboard";
 
@@ -8,3 +18,9 @@ export default function AdminPage() {
     </AdminShell>
   );
 }
+// El Hamdu Lillah Kesira
+
+// La ilahe illALLAH u vahdehu la şerike leh, lehul-mulku ve lehul-hamdu
+// Yuhyi ve yumit
+// Biyadihil xayr
+// ve huve ala kulli şey'in kadir
